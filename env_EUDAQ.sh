9f1a0d708c3a88c5fcf62a2a7a142e65985e261d
root <root@pnuitsup01.(none)>
2013-09-23 15:38:06 +0200

    changes used on pnuitsup01

--- env_EUDAQ.sh
+++ env_EUDAQ.sh
@@ -6,9 +6,9 @@ source /opt/root/v5.34.09/bin/thisroot.sh
 
 
 # add local library
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/freidt/git/eudaq-official/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hipex/itsUp/eudaq-aliceITSupgrade-Explorer1/bin
 
 # LCIO
-export LCIO=/data/freidt/lcio/v02-04-02
-export PATH="$LCIO/tools:$LCIO/bin:$PATH"
-export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
+#export LCIO=/data/freidt/lcio/v02-04-02
+#export PATH="$LCIO/tools:$LCIO/bin:$PATH"
+#export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"

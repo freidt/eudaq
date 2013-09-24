d64d06741021e70113b38101b4134a2cc7e587c9
Test Beam on hipex machine <felix.reidt@cern.ch>
2013-09-24 13:34:58 +0200

    improved the environment script

--- env_EUDAQ.sh
+++ env_EUDAQ.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+username=hipex
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export PATH=/home/${username}/scripts:/usr/sue/bin:/usr/lib64/qt4/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/$usernamehipex/bin
+
+
 #here you may have to add other files depending on version and pc setup
 #this is for a local account with slc6 and afs access
 source /opt/root/v5.34.09/bin/thisroot.sh
 
 
 # add local library
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hipex/itsUp/eudaq-aliceITSupgrade-Explorer1/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hipex/itsUp/$DIR/bin
 
 # LCIO
 #export LCIO=/data/freidt/lcio/v02-04-02

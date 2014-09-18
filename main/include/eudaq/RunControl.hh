147331164a2183be57e99801df86c5d7f2777e27
jgrosseo <jgrosseo@cern.ch>
2014-09-18 13:37:05 +0200

    possibility to run automatized over several config files

--- main/include/eudaq/RunControl.hh
+++ main/include/eudaq/RunControl.hh
@@ -58,6 +58,7 @@ namespace eudaq {
       size_t m_idata, m_ilog;
 	  std::string m_dataaddr, m_logaddr;
       long long m_runsizelimit;
+      bool m_nextconfigonfilelimit;
       bool m_stopping, m_busy, m_producerbusy;
   };
 

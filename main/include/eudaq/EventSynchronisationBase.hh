7a850a12edaff5da1d1a03e36a2e7581fee585db
Felix Reidt <felix.reidt@cern.ch>
2015-02-03 18:03:34 +0100

    Added Fake-TLU support, WARNING - need to remove couts

--- main/include/eudaq/EventSynchronisationBase.hh
+++ main/include/eudaq/EventSynchronisationBase.hh
@@ -8,66 +8,65 @@
 #include <queue>
 // base class for all Synchronization Plugins
 // it is desired to be as modular es possible with this approach.
-// first step is to separate the events from different Producers. 
+// first step is to separate the events from different Producers.
 // then it will be recombined to a new event
 // The Idea is that the user can define a condition that need to be true to define if an event is sync or not
 
 namespace eudaq{
-  
-
-
 
   class DLLEXPORT SyncBase {
-    public:
-     	 typedef std::queue<std::shared_ptr<eudaq::Event>> eventqueue_t ;
+  public:
+    typedef std::queue<std::shared_ptr<eudaq::Event>> eventqueue_t ;
 
 
-	 int AddDetectorElementToProducerQueue(int fileIndex,std::shared_ptr<eudaq::DetectorEvent> dev );
-	 virtual bool SyncFirstEvent();
-	 virtual bool SyncNEvents(size_t N);
-	 virtual bool getNextEvent( std::shared_ptr<eudaq::DetectorEvent>  & ev);
+    int AddDetectorElementToProducerQueue(int fileIndex,std::shared_ptr<eudaq::DetectorEvent> dev );
+    virtual bool SyncFirstEvent();
+    virtual bool SyncNEvents(size_t N);
+    virtual bool getNextEvent( std::shared_ptr<eudaq::DetectorEvent>  & ev);
 
-	 virtual bool compareTLUwithEventQueue(std::shared_ptr<eudaq::Event>& tlu_event,SyncBase::eventqueue_t& event_queue);
-	 virtual bool compareTLUwithEventQueues(std::shared_ptr<eudaq::Event>& tlu_event);
+    virtual bool compareTLUwithEventQueue(std::shared_ptr<eudaq::Event>& tlu_event,SyncBase::eventqueue_t& event_queue);
+    virtual bool compareTLUwithEventQueues(std::shared_ptr<eudaq::Event>& tlu_event);
 
-	 void storeCurrentOrder();
-	 bool Event_Queue_Is_Empty();
-	 bool SubEventQueueIsEmpty(int i);
-	 void event_queue_pop();
-	 void event_queue_pop_TLU_event();
-	 void makeDetectorEvent();
-	 void clearDetectorQueue();
-      /** The empty destructor. Need to add it to make it virtual.
-       */
-	 virtual ~SyncBase() {}
-	 SyncBase();
-	 void addBOREEvent(int fileIndex,const eudaq::DetectorEvent& BOREvent);
-	 void PrepareForEvents();
+    void storeCurrentOrder();
+    bool Event_Queue_Is_Empty();
+    bool SubEventQueueIsEmpty(int i);
+    void event_queue_pop();
+    void event_queue_pop_TLU_event();
+    void makeDetectorEvent();
+    void clearDetectorQueue();
+    /** The empty destructor. Need to add it to make it virtual.
+     */
+    virtual ~SyncBase() {}
+    SyncBase();
+    void addBOREEvent(int fileIndex,const eudaq::DetectorEvent& BOREvent);
+    void PrepareForEvents();
 
-    protected:
+  protected:
 
-		eventqueue_t& getQueuefromId(unsigned producerID);
-		eventqueue_t& getQueuefromId(unsigned fileIndex,unsigned eventIndex);
-	
-		eventqueue_t& getFirstTLUQueue();
-		unsigned getUniqueID(unsigned fileIndex,unsigned eventIndex);
-		unsigned getTLU_UniqueID(unsigned fileIndex);
-		std::map<unsigned,size_t> m_ProducerId2Eventqueue;
-		size_t m_registertProducer;
-		std::vector<size_t> m_EventsProFileReader;
-		/* This vector saves for each producer an event queue */
-		
-		std::vector<eventqueue_t> m_ProducerEventQueue;
-		
-		std::queue<std::shared_ptr<eudaq::DetectorEvent>> m_DetectorEventQueue;
-	
+    eventqueue_t& getQueuefromId(unsigned producerID);
+    eventqueue_t& getQueuefromId(unsigned fileIndex,unsigned eventIndex);
 
-		int m_TLUs_found;
+    eventqueue_t& getFirstTLUQueue();
+    unsigned getUniqueID(unsigned fileIndex,unsigned eventIndex);
+    unsigned getTLU_UniqueID(unsigned fileIndex);
+    std::map<unsigned,size_t> m_ProducerId2Eventqueue;
+    size_t m_registertProducer;
+    std::vector<size_t> m_EventsProFileReader;
+    /* This vector saves for each producer an event queue */
 
-		bool isAsync_;
-		size_t NumberOfEventsToSync_;
-		unsigned long long longTimeDiff_;
-		
+    std::vector<eventqueue_t> m_ProducerEventQueue;
+
+    std::queue<std::shared_ptr<eudaq::DetectorEvent>> m_DetectorEventQueue;
+
+    int m_TLUs_found;
+    int m_event_counter;
+    int m_run_number;
+    bool m_fake_TLU;
+
+    bool isAsync_;
+    size_t NumberOfEventsToSync_;
+    unsigned long long longTimeDiff_;
+
   };
 
 }//namespace eudaq
--- main/lib/src/EventSynchronisationBase.cc
+++ main/lib/src/EventSynchronisationBase.cc
@@ -14,328 +14,289 @@ using std::endl;
 using std::shared_ptr;
 using namespace std;
 namespace eudaq{
-SyncBase::SyncBase():
-	m_registertProducer(0),
-	m_ProducerEventQueue(0),
-	NumberOfEventsToSync_(1),longTimeDiff_(0),isAsync_(false),m_TLUs_found(0)
-{
+  SyncBase::SyncBase()
+    : m_registertProducer(0)
+    , m_ProducerEventQueue(0)
+    , NumberOfEventsToSync_(1)
+    , longTimeDiff_(0)
+    , isAsync_(false)
+    , m_TLUs_found(0)
+    , m_event_counter(0)
+    , m_run_number(0)
+    , m_fake_TLU(false)
+  {
+  }
 
+  void SyncBase::addBOREEvent(int fileIndex, const eudaq::DetectorEvent& BOREvent )
+  {
+    std::cout << std::endl << std::endl << "addBOREEvent" << std::endl << std::endl << std::endl;
+    m_registertProducer+=BOREvent.NumEvents();
+    m_EventsProFileReader.push_back(BOREvent.NumEvents());
+    std::cout << "BOREvent.NumEvents()=" << BOREvent.NumEvents() << std::endl;
+    eudaq::Configuration conf(BOREvent.GetTag("CONFIG"));
+    conf.SetSection("EventStruct");
+    longTimeDiff_=conf.Get("LongBusyTime",longTimeDiff_); //from config file
+    longTimeDiff_=BOREvent.GetTag("longTimeDelay",longTimeDiff_);//from command line
+    NumberOfEventsToSync_=conf.Get("NumberOfEvents",NumberOfEventsToSync_); //from config file
+    NumberOfEventsToSync_=BOREvent.GetTag("NumberOfEvents",NumberOfEventsToSync_);//from command line
+    const unsigned int TLU_ID=Event::str2id("_TLU");
+    static size_t id=1;
+    for (unsigned i=0;i<BOREvent.NumEvents();++i)
+    {
+      if (TLU_ID==BOREvent.GetEvent(i)->get_id())
+      {
+        if (m_TLUs_found==0)
+        {
+          m_ProducerId2Eventqueue[getUniqueID(fileIndex,i)]=0;
+        }else{
+          m_ProducerId2Eventqueue[getUniqueID(fileIndex,i)]=id++; //only the first TLU gets threated differently all others are just producers
+        }
+        ++m_TLUs_found;
+      }else{
+        m_ProducerId2Eventqueue[getUniqueID(fileIndex,i)]=id++;
+      }
+    }
+    if (m_TLUs_found==0) {
+      std::cerr << "No TLU found, faking a TLU" << std::endl;
+      m_fake_TLU = true;
+      m_run_number = BOREvent.GetRunNumber();
+      m_EventsProFileReader.back()=BOREvent.NumEvents()+1;
+      ++m_registertProducer;
+      ++m_TLUs_found;
+    }
+  }
 
-}
+  bool SyncBase::Event_Queue_Is_Empty()
+  {
+    for (auto q=m_ProducerEventQueue.begin();q!=m_ProducerEventQueue.end();++q)
+    {
+      if (q->empty())
+      {
+        return true;
+      }
+    }
+    return false;
+  }
 
+  bool SyncBase::SubEventQueueIsEmpty( int FileID )
+  {
+    for (size_t eventNR=0;eventNR<m_EventsProFileReader[FileID];++eventNR)
+    {
+      if(getQueuefromId(FileID,eventNR).empty()){
+        return true;
+      }
+    }
+    return false;
+  }
 
-
-
-void SyncBase::addBOREEvent(int fileIndex, const eudaq::DetectorEvent& BOREvent )
-{
-	m_registertProducer+=BOREvent.NumEvents();
-
-	m_EventsProFileReader.push_back(BOREvent.NumEvents());
-	
-	eudaq::Configuration conf(BOREvent.GetTag("CONFIG"));
-	conf.SetSection("EventStruct");
-
-
-
-
-	longTimeDiff_=conf.Get("LongBusyTime",longTimeDiff_); //from config file
-	longTimeDiff_=BOREvent.GetTag("longTimeDelay",longTimeDiff_);//from command line
-
-	NumberOfEventsToSync_=conf.Get("NumberOfEvents",NumberOfEventsToSync_); //from config file
-	NumberOfEventsToSync_=BOREvent.GetTag("NumberOfEvents",NumberOfEventsToSync_);//from command line
-
-
-	const unsigned int TLU_ID=Event::str2id("_TLU");
-	static size_t id=1;
-	
-	for (unsigned i=0;i<BOREvent.NumEvents();++i)
-	{
-		if (TLU_ID==BOREvent.GetEvent(i)->get_id())
-		{
-			if (m_TLUs_found==0)
-			{
-				m_ProducerId2Eventqueue[getUniqueID(fileIndex,i)]=0;
-			}else{
-			 m_ProducerId2Eventqueue[getUniqueID(fileIndex,i)]=id++; //only the first TLU gets threated differently all others are just producers
-			}
-			 ++m_TLUs_found;
-		}else{
-
-			m_ProducerId2Eventqueue[getUniqueID(fileIndex,i)]=id++;
-		}
-		
-	}
-
-}
-
-bool SyncBase::Event_Queue_Is_Empty()
-{
-	
-	for (auto q=m_ProducerEventQueue.begin();q!=m_ProducerEventQueue.end();++q)
-	{
-		if (q->empty())
-		{
-			return true;
-		}
-	}
-	return false;
-}
-
-bool SyncBase::SubEventQueueIsEmpty( int FileID )
-{
-	for (size_t eventNR=0;eventNR<m_EventsProFileReader[FileID];++eventNR)
-	{
-		if(getQueuefromId(FileID,eventNR).empty()){
-			return true;
-		}
-	}
-	return false;
-}
-
-
-
-bool SyncBase::getNextEvent(  std::shared_ptr<eudaq::DetectorEvent>  & ev )
-{
-	
+  bool SyncBase::getNextEvent(  std::shared_ptr<eudaq::DetectorEvent>  & ev )
+  {
 		if (!m_DetectorEventQueue.empty())
 		{
-			
 			ev=m_DetectorEventQueue.front();
 			m_DetectorEventQueue.pop();
 			return true;
 		}
-	return false;
-}
+    return false;
+  }
 
-SyncBase::eventqueue_t& SyncBase::getQueuefromId(unsigned producerID )
-{
-	if(m_ProducerId2Eventqueue.find(producerID)==m_ProducerId2Eventqueue.end()){
+  SyncBase::eventqueue_t& SyncBase::getQueuefromId(unsigned producerID )
+  {
+    if(m_ProducerId2Eventqueue.find(producerID)==m_ProducerId2Eventqueue.end()){
+      std::cout << "producerID " << producerID << " not found" << std::endl;
+      EUDAQ_THROW("unknown Producer ID");
+    }
+    return m_ProducerEventQueue[m_ProducerId2Eventqueue[producerID]];
+  }
 
-		EUDAQ_THROW("unknown Producer ID");
- 	}
+  SyncBase::eventqueue_t& SyncBase::getQueuefromId( unsigned fileIndex,unsigned eventIndex )
+  {
+    return getQueuefromId(getUniqueID(fileIndex,eventIndex));
+  }
 
-	return m_ProducerEventQueue[m_ProducerId2Eventqueue[producerID]];
-}
+  SyncBase::eventqueue_t& SyncBase::getFirstTLUQueue()
+  {
+    return m_ProducerEventQueue[0];
+  }
 
-SyncBase::eventqueue_t& SyncBase::getQueuefromId( unsigned fileIndex,unsigned eventIndex )
-{
-	return getQueuefromId(getUniqueID(fileIndex,eventIndex));
-}
+  bool SyncBase::SyncFirstEvent()
+  {
+    if (m_ProducerEventQueue.size()==0)
+    {
+      EUDAQ_THROW("Producer Event queue is empty");
+    }
+    auto& TLU_queue=getFirstTLUQueue();
+    while (!Event_Queue_Is_Empty())
+    {
 
-SyncBase::eventqueue_t& SyncBase::getFirstTLUQueue()
-{
-	return m_ProducerEventQueue[0];
-}
+      if (compareTLUwithEventQueues(TLU_queue.front()))
+      {
+        std::cout << "making new detector event" << std::endl;
+        makeDetectorEvent();
+        return true;
+      }else if(!Event_Queue_Is_Empty())
+      {
+        TLU_queue.pop();
+      }
+    }
+    return false;
+  }
 
-
-
-
-bool SyncBase::SyncFirstEvent()
-{
-
-	if (m_ProducerEventQueue.size()==0)
-	{
-		
-		EUDAQ_THROW("Producer Event queue is empty");
-	}
-	auto& TLU_queue=getFirstTLUQueue();
-
-
-
-
-
-	while (!Event_Queue_Is_Empty())
-	{
-		
-		if (compareTLUwithEventQueues(TLU_queue.front()))
-		{
-			makeDetectorEvent();
-			return true;
-		}else if(!Event_Queue_Is_Empty())
-		{
-			TLU_queue.pop();
-		}
-		
-	
-	}
-
-	
-	
-
-
-	return false;
-}
-
-bool SyncBase::SyncNEvents( size_t N )
-{
-	while (m_DetectorEventQueue.size()<=N)
-	{
-		if (!SyncFirstEvent())
-		{
-			return false;
-		}
+  bool SyncBase::SyncNEvents( size_t N )
+  {
+    std::cout << "SyncNEvents, N=" << N << ", "<< m_DetectorEventQueue.size() <<std::endl;
+    while (m_DetectorEventQueue.size()<=N)
+    {
+      std::cout << "actually starting to sync" << std::endl;
+      if (!SyncFirstEvent())
+      {
+        return false;
+      }
 // 		if (isAsync_)
 // 		{
 // 			isAsync_=false;
 // 			auto last_element=m_DetectorEventQueue.back(); //buffering the last event. because this is sync.
-// 		
-// 		
+//
+//
 // 			clearDetectorQueue();
-// 		
+//
 // 			m_DetectorEventQueue.push(last_element);
 // 		}
-	}
-	return true;
-}
+    }
+    return true;
+  }
 
-void SyncBase::makeDetectorEvent()
-{
-	auto &TLU=m_ProducerEventQueue[0].front();
-	shared_ptr<DetectorEvent> det=make_shared<DetectorEvent>(TLU->GetRunNumber(),TLU->GetEventNumber(),TLU->GetTimestamp());
-	det->AddEvent(TLU);
-	for (size_t i=1;i<m_ProducerEventQueue.size();++i)
-	{
+  void SyncBase::makeDetectorEvent()
+  {
+    auto &TLU=m_ProducerEventQueue[0].front();
+    shared_ptr<DetectorEvent> det=make_shared<DetectorEvent>(TLU->GetRunNumber(),TLU->GetEventNumber(),TLU->GetTimestamp());
+    det->AddEvent(TLU);
+    for (size_t i=1;i<m_ProducerEventQueue.size();++i)
+    {
+      det->AddEvent(m_ProducerEventQueue[i].front());
+    }
+    m_DetectorEventQueue.push(det);
+    //event_queue_pop_TLU_event();
+    event_queue_pop();
+  }
 
-		det->AddEvent(m_ProducerEventQueue[i].front());
-
-	}
-
-	
-	m_DetectorEventQueue.push(det);
-	//event_queue_pop_TLU_event();
-	event_queue_pop();
-	
-}
-
-void SyncBase::event_queue_pop()
-{
-	for (auto q=m_ProducerEventQueue.begin();q!=m_ProducerEventQueue.end();++q)
-	{
-		
+  void SyncBase::event_queue_pop()
+  {
+    for (auto q=m_ProducerEventQueue.begin();q!=m_ProducerEventQueue.end();++q)
+    {
 			q->pop();
-		
-	}
+    }
+  }
 
-}
+  void SyncBase::event_queue_pop_TLU_event()
+  {
+    m_ProducerEventQueue.begin()->pop();
+  }
 
+  bool SyncBase::compareTLUwithEventQueue( std::shared_ptr<eudaq::Event>& tlu_event,eudaq::SyncBase::eventqueue_t& event_queue )
+  {
+    int ReturnValue=Event_IS_Sync;
 
+    std::shared_ptr<TLUEvent> tlu=std::dynamic_pointer_cast<TLUEvent>(tlu_event);
 
-void SyncBase::event_queue_pop_TLU_event()
-{
-	m_ProducerEventQueue.begin()->pop();
-}
+    while(!event_queue.empty())
+    {
+      auto& currentEvent=event_queue.front();
+      ReturnValue=PluginManager::IsSyncWithTLU(*currentEvent,*tlu);
+      std::cout << "ReturnValue=" << ReturnValue << std::endl;
+      if (ReturnValue== Event_IS_Sync )
+      {
+        return true;
+      }
+      else if (ReturnValue==Event_IS_LATE)
+      {
+        isAsync_=true;
+        event_queue.pop();
+      }else if (ReturnValue==Event_IS_EARLY)
+      {
+        isAsync_=true;
+        return false;
+      }
+    }
+    return false;
+  }
 
-bool SyncBase::compareTLUwithEventQueue( std::shared_ptr<eudaq::Event>& tlu_event,eudaq::SyncBase::eventqueue_t& event_queue )
-{
-	int ReturnValue=Event_IS_Sync;
+  bool SyncBase::compareTLUwithEventQueues( std::shared_ptr<eudaq::Event>& tlu_event )
+  {
+    for (size_t i=1;i<m_ProducerEventQueue.size();++i)
+    {
+      if(!compareTLUwithEventQueue( tlu_event,m_ProducerEventQueue[i])){
+        // could not sync event.
+        // TLU event is to early or event queue is empty;
+        return false;
+      }
+    }
+    return true;
+  }
 
-	std::shared_ptr<TLUEvent> tlu=std::dynamic_pointer_cast<TLUEvent>(tlu_event);
-
-	
-
-	while(!event_queue.empty())
-	{
-		auto& currentEvent=event_queue.front();
-
-		ReturnValue=PluginManager::IsSyncWithTLU(*currentEvent,*tlu);
-		if (ReturnValue== Event_IS_Sync )
-		{
-			
-			return true;
-			
-		}
-		else if (ReturnValue==Event_IS_LATE)
-		{
-		
-			isAsync_=true;
-			event_queue.pop();
-
-			
-		}else if (ReturnValue==Event_IS_EARLY)
-		{
-		    isAsync_=true;
-			return false;
-		}
-
-
-
-	}
-	return false;
-}
-
-bool SyncBase::compareTLUwithEventQueues( std::shared_ptr<eudaq::Event>& tlu_event )
-{
-	for (size_t i=1;i<m_ProducerEventQueue.size();++i)
-	{
-		if(!compareTLUwithEventQueue( tlu_event,m_ProducerEventQueue[i])){
-			// could not sync event.
-			// TLU event is to early or event queue is empty;
-			return false;
-		}
-	}
-	return true;
-}
-
-void SyncBase::clearDetectorQueue()
-{
-	
+  void SyncBase::clearDetectorQueue()
+  {
 		std::queue<std::shared_ptr<eudaq::DetectorEvent>> empty;
 		std::swap( m_DetectorEventQueue, empty );
+  }
 
-}
+  void SyncBase::PrepareForEvents()
+  {
+    std::cout << "Bla" << std::endl;
+    std::cout << m_registertProducer << std::endl;
+    std::cout << &m_DetectorEventQueue << std::endl;
+    std::cout << m_ProducerId2Eventqueue.size() << std::endl;
+    size_t max_events = 0;
+    for (auto iter=m_ProducerId2Eventqueue.begin(); iter!=m_ProducerId2Eventqueue.end(); ++iter) {
+      std::cout << iter->first << " " << iter->second << std::endl;
+      size_t t=m_ProducerEventQueue[iter->second].size();
+      if (t>max_events) max_events=t;
+    }
+    if (m_TLUs_found==0 || m_fake_TLU)
+    {
+      std::cout << "no TLU events found in the data\n for the resynchronisation it is nessasary to have a TLU in the data stream \n for now the synchrounsation only works with the old TLU (date 12.2013)" << std::endl;
+      std::cout << "generating TLU event stream starting from 0" << std::endl;
+    }else if (m_TLUs_found>1)
+    {
+      //EUDAQ_THROW("to many TLUs in the data stream.\n the sync mechanism only works with 1 TLU");
+      std::cout<< "more than one TLU detected only the first TLU is used for synchronisation "<<std::endl;
+    }
+    m_ProducerEventQueue.resize(m_registertProducer);
+  }
 
-void SyncBase::PrepareForEvents()
-{
-	if (m_TLUs_found==0)
-	{
-		EUDAQ_THROW("no TLU events found in the data\n for the resynchronisation it is nessasary to have a TLU in the data stream \n for now the synchrounsation only works with the old TLU (date 12.2013)");
-	}else if (m_TLUs_found>1)
-	{
-		//EUDAQ_THROW("to many TLUs in the data stream.\n the sync mechanism only works with 1 TLU");
-		std::cout<< "more than one TLU detected only the first TLU is used for synchronisation "<<std::endl;
-	}
-	m_ProducerEventQueue.resize(m_registertProducer);
-}
+  unsigned SyncBase::getUniqueID( unsigned fileIndex,unsigned eventIndex )
+  {
+    return fileIndex*10000+eventIndex;
+  }
 
-unsigned SyncBase::getUniqueID( unsigned fileIndex,unsigned eventIndex )
-{
-	return fileIndex*10000+eventIndex;
-}
+  unsigned SyncBase::getTLU_UniqueID( unsigned fileIndex )
+  {
+    return getUniqueID(0,fileIndex);
+  }
 
-unsigned SyncBase::getTLU_UniqueID( unsigned fileIndex )
-{
-	return getUniqueID(0,fileIndex);
-}
+  void SyncBase::storeCurrentOrder()
+  {
+    shared_ptr<TLUEvent> tlu=std::dynamic_pointer_cast<TLUEvent>(getFirstTLUQueue().back());
+    for (size_t i=1; i<m_ProducerEventQueue.size();++i)
+    {
+      shared_ptr<Event> currentEvent=m_ProducerEventQueue[i].back();
+      eudaq::PluginManager::setCurrentTLUEvent(*currentEvent,*tlu);
+    }
+  }
 
-void SyncBase::storeCurrentOrder()
-{
-	shared_ptr<TLUEvent> tlu=std::dynamic_pointer_cast<TLUEvent>(getFirstTLUQueue().back());
-	for (size_t i=1; i<m_ProducerEventQueue.size();++i)
-	{
-		shared_ptr<Event> currentEvent=m_ProducerEventQueue[i].back();
-		eudaq::PluginManager::setCurrentTLUEvent(*currentEvent,*tlu);
-	}
-	
-}
-
-int SyncBase::AddDetectorElementToProducerQueue( int fileIndex,std::shared_ptr<eudaq::DetectorEvent> detEvent )
-{
-	if (detEvent)
-	{
-	
-	
-
-
-			
+  int SyncBase::AddDetectorElementToProducerQueue( int fileIndex,std::shared_ptr<eudaq::DetectorEvent> detEvent )
+  {
+    if (detEvent)
+    {
 			for(size_t i=0;i< detEvent->NumEvents();++i){
-			
 				auto &q=getQueuefromId(fileIndex,i);
+        std::cout << i << " " <<  &q << " " << q.size() << " " << fileIndex << std::endl;
 				q.push(detEvent->GetEventPtr(i));
 			}
-
-		
-	}
-	return true;
-}
-
+      if (m_fake_TLU) {
+        getFirstTLUQueue().push(std::make_shared<TLUEvent>(TLUEvent(m_run_number, m_event_counter++, 0ULL)));
+      }
+    }
+    return true;
+  }
 }

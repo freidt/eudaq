198f0a032588176952b8d9432bbcef97e4b91dfd
Felix Reidt <felix.reidt@cern.ch>
2017-08-02 15:40:39 +0200

    changing the CI settings

--- .travis/install_palpidefs_driver.sh
+++ .travis/install_palpidefs_driver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This package is necessary for the palpidefs producer
+# This package is necessary for the alpide producer
 
-echo "Entering install_palpidefs_driver"
-echo "Installing palpidefs driver:"
+echo "Entering install_alpide_driver"
+echo "Installing alpide driver:"
 
 export temporary_path=`pwd`
 
@@ -11,93 +11,37 @@ echo $temporary_path
 
 cd $TRAVIS_BUILD_DIR/extern/
 
-if [ $TRAVIS_OS_NAME == linux ]; then 
-	
-	sudo apt-get update && sudo apt-get install -y libtinyxml-dev expect-dev libusb-1.0-0-dev; 
+if [ $TRAVIS_OS_NAME == linux ]; then
 
-	if [ -d "$TRAVIS_BUILD_DIR/extern/aliceitsalpidesoftware/pALPIDEfs-software" ]; then
-	
-		echo "palpidefs source restored from cache as path exists:"
-		
-		zip -r aliceitsalpidesoftware.zip aliceitsalpidesoftware
-		
+	sudo apt-get update && sudo apt-get install -y libtinyxml-dev expect-dev libusb-1.0-0-dev;
+
+	if [ -d "$TRAVIS_BUILD_DIR/extern/new-alpide-software" ]; then
+
+		echo "new-alpide-software source restored from cache as path exists:"
+
+		zip -r new-alpide-software.zip new-alpide-software
+
 	else
-		echo "palpidefs source not restored from cache - downloading from CERNBOX and unpacking"
-		
-		wget -O alice-its-alpide-software.zip https://cernbox.cern.ch/index.php/s/WpOQOjrUXihXD98/download
-		
-		unzip alice-its-alpide-software.zip
-	
-		rm -rf aliceitsalpidesoftware
+		echo "new-alpide-software source not restored from cache - downloading from CERNBOX and unpacking"
 
-		mv alice-its-alpide-software-* aliceitsalpidesoftware
-	
-		zip -r aliceitsalpidesoftware.zip aliceitsalpidesoftware
+        mkdir new-alpide-software
+
+		wget -O new-alpide-software.zip https://cernbox.cern.ch/index.php/s/TN3BQ8FsOtocpP6/download
+
+		unzip new-alpide-software.zip
+
+		mv new-alpide-software.zip ../
 	fi
 
 	echo `ls `
-	
-	cd aliceitsalpidesoftware
-	cd pALPIDEfs-software
 
-	sed -i '2s/.*//' Makefile
-	
+	cd new-alpide-software
 	make lib
 
-fi
-
-# http://apple.stackexchange.com/questions/193138/to-install-unbuffer-in-osx
-
-if [ $TRAVIS_OS_NAME == osx ]; then 
-
-	brew update
-	#brew --prefix
-	#brew -ls verbose tinyxml
-	#brew install tinyxml homebrew/dupes/expect libusb
-	brew install homebrew/dupes/expect libusb
-
-	wget -O tinyxml_2_6_2.zip http://downloads.sourceforge.net/project/tinyxml/tinyxml/2.6.2/tinyxml_2_6_2.zip
-
-	unzip tinyxml_2_6_2.zip
-
-	cd tinyxml
-
-	make
-	
-	export PATH=/Users/travis/tinyxml:$PATH
-	export CFLAGS="-I /Users/travis/tinyxml" $CFLAGS
-	export LDFLAGS="-L /Users/travis/tinyxml" $LDFLAGS
-	
-	mv /Users/travis/tinyxml/tinystr.o /Users/travis/tinyxml/tinystr.a
-	mv /Users/travis/tinyxml/tinyxmlerror.o /Users/travis/tinyxml/tinyxmlerror.a
-	mv /Users/travis/tinyxml/tinyxmlparser.o /Users/travis/tinyxml/tinyxmlparser.a
-	mv /Users/travis/tinyxml/tinyxml.o /Users/travis/tinyxml/tinyxml.a
-	
-	cd ..	
-
-	if [ ! -d $TRAVIS_BUILD_DIR/extern/aliceitsalpidesoftware ]; then
-		wget -O alice-its-alpide-software.zip https://cernbox.cern.ch/index.php/s/WpOQOjrUXihXD98/download
-
-		unzip alice-its-alpide-software.zip
-		mv alice-its-alpide-software-* aliceitsalpidesoftware
-		zip -r aliceitsalpidesoftware.zip aliceitsalpidesoftware
-	fi
-
-	cd aliceitsalpidesoftware
-	cd pALPIDEfs-software
-
-	sed -i '' '2s/.*/GIT_VERSION:=\"e1b12f7\"/' Makefile
-	sed -i '' '3s/.*/CFLAGS= -I\/Users\/travis\/tinyxml -pipe -fPIC -DVERSION=\\"$(GIT_VERSION)\\" -g -std=c++11/' Makefile
-	sed -i '' '4s/.*/LINKFLAGS=\/Users\/travis\/tinyxml\/tinystr.a \/Users\/travis\/tinyxml\/tinyxmlerror.a \/Users\/travis\/tinyxml\/tinyxmlparser.a \/Users\/travis\/tinyxml\/tinyxml.a -lusb-1.0 /' Makefile		
-	
-	sed -i '' '308s/.*//' c_wrapper.cpp		
-	
-	make lib
-	
 fi
 
 pwd
 
 cd $temporary_path
 
-echo "Installed palpidefs driver"
+echo "Installed ALPIDE driver"

c456cf4631cf23b9e7e360fb6d202e5b896cbd65
Felix Reidt <felix.reidt@cern.ch>
2017-08-07 17:00:02 +0200

    working on the ALPIDE driver install script

--- .travis/install_alpide_driver.sh
+++ .travis/install_alpide_driver.sh
@@ -5,38 +5,40 @@
 echo "Entering install_alpide_driver"
 echo "Installing alpide driver:"
 
-export temporary_path=`pwd`
+export temporary_path="$(pwd)"
 
 echo $temporary_path
 
-cd $TRAVIS_BUILD_DIR/extern/
+cd "$TRAVIS_BUILD_DIR/extern/"
 
-if [ $TRAVIS_OS_NAME == linux ]; then
+if [[ $TRAVIS_OS_NAME == linux ]]; then
 
-	sudo apt-get update && sudo apt-get install -y libtinyxml-dev expect-dev libusb-1.0-0-dev;
+    sudo apt-get update && sudo apt-get install -y libtinyxml-dev expect-dev libusb-1.0-0-dev;
 
-	if [ -f "$TRAVIS_BUILD_DIR/extern/new-alpide-software/libalpide.so" ]; then
+    if [[ -f "$TRAVIS_BUILD_DIR/extern/new-alpide-software/libalpide.so" ]]; then
 
-		echo "new-alpide-software source restored from cache as path exists:"
+        echo "new-alpide-software source restored from cache as path exists:"
 
-		zip -r new-alpide-software.zip new-alpide-software
+        zip -r new-alpide-software.zip new-alpide-software
 
-	else
-		echo "new-alpide-software source not restored from cache - downloading from CERNBOX and unpacking"
+    else
+        echo "new-alpide-software source not restored from cache - downloading from CERNBOX and unpacking"
 
-		wget -O new-alpide-software.zip https://cernbox.cern.ch/index.php/s/TN3BQ8FsOtocpP6/download
+        wget -O new-alpide-software.zip https://cernbox.cern.ch/index.php/s/TN3BQ8FsOtocpP6/download
 
-		unzip new-alpide-software.zip
+        mkdir new-alpide-software
 
-	    cd new-alpide-software
+        cd new-alpide-software
+
+        unzip ../new-alpide-software.zip
 
         make lib
-	fi
+    fi
 
 fi
 
 pwd
 
-cd $temporary_path
+cd "$temporary_path"
 
 echo "Installed ALPIDE driver"
